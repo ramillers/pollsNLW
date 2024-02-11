7f7fef58ec30ee6d9935505047b6934db93707f6
ramillers <rmillesantana@gmail.com>
2024-02-11 00:03:12 -0300

    feat: integrate "zod" library with TypeScript

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,20 @@
 import fastify from "fastify"; 
+import { z } from 'zod'; 
+import { request } from "http";
 
 const app = fastify()
 
-//qnd usuário acessar endereço "hello" vai executar a função
-app.get('/hello', () => {
-    return 'Hello, Ramille! '
+//qnd usuário acessar endereço "polls" vai executar a função
+app.post('/polls', (request) => {
+    //variavel de validação do request body
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body) //verifica se o request body tá no formato de objeto
+    //zod retorna as infos tipadas 
+
+    return 'Hello, Ramille!'
 })
 
 app.listen({ port: 3333}).then(() => (
